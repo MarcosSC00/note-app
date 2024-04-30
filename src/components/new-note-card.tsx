d17fc593610360971d9891fb73ffcf1ba762156b
Marcos <msilvachaves02@gmail.com>
2024-04-30 10:21:48 -0300

    changes in new-note-card

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -105,7 +105,7 @@ export function NewNoteCard({onNoteCreated}:NewNoteCardsProps) {
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Content className="fixed  bg-slate-700 flex flex-col w-full overflow-hidden inset-0 md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:h-[60vh] md:rounded-md">
+        <Dialog.Content className="fixed bg-slate-600 flex flex-col w-full overflow-hidden inset-0 md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:h-[60vh] md:rounded-md">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-300 hover:text-slate-50">
             <X className="size-5" />
           </Dialog.Close>
@@ -135,7 +135,7 @@ export function NewNoteCard({onNoteCreated}:NewNoteCardsProps) {
             </div>
 
             {isRecording ? (
-              <button className='flex items-center justify-center gap-2 bg-slate-900 w-full p-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100'
+              <button className='flex items-center justify-center gap-2 bg-slate-800 w-full p-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100'
               onClick={handleStopRecording}>
               <div className="size-3 rounded-full bg-red-500 animate-pulse" />
               Gravando! (clique aqui para interromper)
